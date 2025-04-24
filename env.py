89c6d53a5dba682261c63ea04827313383478255
curran68 <mac1968@gmail.com>
2025-04-24 17:11:29 +0100

    final changes

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "QM2QXTlCvp(#(=Y")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://mac1968:zxc123@clustermc.eowgl.mongodb.net/recipe_manager?retryWrites=true&w=majority&appName=ClusterMC")
-os.environ.setdefault("MONGO_DBNAME", "recipes")
